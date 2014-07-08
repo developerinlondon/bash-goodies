3db05bb8736bc347b1a15c0bb4aae5219523ddf1
Nayeem Syed <developerinlondon@gmail.com>
2014-07-08 14:36:16 +0100

    Update measure_time.sh

--- measure_time.sh
+++ measure_time.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash -x
 start_time=`date +%s%3N`
 sleep(10)
+#
+# do some stuff here
+#
 stop_time=`date +%s%3N`
 total_time=`expr $start_time - $stop_time`
 echo $(awk "BEGIN {printf \"Total Time: %dh:%dm:%0.3fs\",$total_time/3600000,$total_time/1000%3600/60,($total_time/1000)%60}")
